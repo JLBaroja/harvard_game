71432a4b803a7a55ee7bb71addeb87b54c209892
Lab25 <lab25@localhost.localdomain>
2017-03-11 19:06:57 -0600

    Seems first simulations with WSLS and RNDM work

--- reinf_learn_Hgame.R
+++ reinf_learn_Hgame.R
@@ -7,16 +7,18 @@ plot(lt_parameters,ylim=c(0,1),pch=21,bg='black',type='o',cex=2)
 points(st_parameters,pch=21,bg='white',type='o',cex=2)
 
 
-havard_game <- function(choice) {
+
+harvard_game <- function(choice) {
+  possible_outcomes <- c('loss','win')
   
   if(choice=='short_term'){
-    result <- rbinom(1,size=1,prob=st_parameters[pos])
+    result <- possible_outcomes[rbinom(1,size=1,prob=st_parameters[pos])+1]
     if(pos!=1){
       pos <- pos-1
     }
   }
   else if(choice=='long_term'){
-    result <- rbinom(1,size=1,prob=lt_parameters[pos])
+    result <- possible_outcomes[rbinom(1,size=1,prob=lt_parameters[pos])+1]
     if(pos!=11){
       pos <- pos+1
     }
@@ -26,32 +28,66 @@ havard_game <- function(choice) {
 }
 
 
+
 wsls <- function(previous_choice,
                  previous_result,
                  trial,
                  theta) {
-
+  
   if(trial!=1){
     if(previous_result=='win'){
-      next_choice <- previous_choice
+      next_choice <- sample(c(previous_choice,possible_choices[which(possible_choices!=previous_choice)]),
+                            size=1,prob=c(theta,1-theta))
     }
-    else{
-      next_choice <- possible_choices[which(possible_choices!=previous_choice)]
+    else if(previous_result=='loss'){
+      next_choice <- sample(c(previous_choice,possible_choices[which(possible_choices!=previous_choice)]),
+                            size=1,prob=c(1-theta,theta))
     }
   }  
   else{
-    next_choice <- rbinom(1,size=1,prob=theta)
+    next_choice <- possible_choices[rbinom(1,size=1,prob=theta)+1]
   }
   
+  return(next_choice)
 }
+
+
+
+rndm <- function(theta){
+  next_choice <- possible_choices[rbinom(1,size=1,prob=theta)+1]
+  return(next_choice)
+}
+
 
 
 # previous_choice <- 'short_term'
 
+
 # Global Variables
+n_trials <- 2000
+sims_per_parameter <- 100
 possible_choices <- c('short_term','long_term')
 pos <- 5 # starting indexing of alternatives' vectors
 
+wsls_results <- array(dim=c(sims_per_parameter,n_trials))
+wsls_choices <- array(dim=c(sims_per_parameter,n_trials))
+rndm_results <- array(dim=c(sims_per_parameter,n_trials))
+rndm_choices <- array(dim=c(sims_per_parameter,n_trials))
+
+for(spp in 1:sims_per_parameter){
+  previous_choice_wsls <- NULL
+  previous_result_wsls <- NULL
+  for(trial in 1:n_trials){
+    wsls_choices[spp,trial] <- wsls(previous_choice=previous_choice_wsls,
+                                    previous_result=previous_result_wsls,
+                                    trial=trial,theta=.8)
+    rndm_choices[spp,trial] <- rndm(theta=.8)
+    wsls_results[spp,trial] <- harvard_game(wsls_choices[spp,trial])
+    rndm_results[spp,trial] <- harvard_game(rndm_choices[spp,trial])
+    previous_choice_wsls <- wsls_choices[spp,trial]
+    previous_result_wsls <- wsls_results[spp,trial]
+  }
+}
 
 
 
