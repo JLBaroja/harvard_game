517421929bd04260e911f60415dbb3ae34764501
Lab25 <lab25@localhost.localdomain>
2017-04-14 21:57:06 -0500

    Recording Z-pulses' in real time

--- Experiment Pigeons/Code/testing_MED_output.R
+++ Experiment Pigeons/Code/testing_MED_output.R
@@ -21,7 +21,7 @@ add_event <- function(h_event,
 
 interval_event <- function(event_name,
                            y_coord,
-                           bw=.1,
+                           bw=.07,
                            lab_on='_on',
                            lab_off='_off',
                            color='#0055ff66') {
@@ -41,20 +41,21 @@ interval_event <- function(event_name,
     
   }
   
-  add_event(name_on,y_coord=y_coord+bw*.8,pch=24,bg=color) 
-  add_event(name_off,y_coord=y_coord-bw*.8,pch=25,bg=color) 
+  add_event(name_on,y_coord=y_coord+bw*.8,pch=25,bg=color) 
+  add_event(name_off,y_coord=y_coord-bw*.8,pch=24,bg=color) 
   
-  text(0,y_coord,event_name,adj=1,cex=1.5)
+  text(-max(td$session_time)*.01,y_coord,event_name,adj=1,cex=1.5)
 }
 
   
 setwd(results_folder)
 # pdf(file='test_results.pdf',width=15,height=5)
 # dev.new(width=5,height=3)
-x11(width=20,height=5)
-par(bg='#999999',col.axis='#555555',fg='#777777')
+x11(width=20,height=7)
+par(bg='#eeeeee',col.axis='#555555',fg='#333333',
+    mar=c(3,1,3,1))
 plot(0,type='n',
-     xlim=c(-max(td$session_time)*.05,
+     xlim=c(-max(td$session_time)*.07,
             max(td$session_time)),
      ann=F,axes=F)
 axis(1,at=0:tail(ceiling(td$session_time),1),cex.axis=.6)
@@ -66,9 +67,9 @@ interval_event('central_light',0,color = '#00cc77cc')
 interval_event('mel_light',0.4,color = '#33cc00cc')  
 interval_event('max_light',-0.4,color = '#33cc00cc')  
 interval_event('feeder',-.85,color = '#ee1100cc')  
-add_event('resp_max_key',.4,pch=4,cex=1.5,lwd=2)
-add_event('resp_central_key',0,pch=4,cex=1.5,lwd=2)
-add_event('resp_mel_key',-.4,pch=4,cex=1.5,lwd=2)
+add_event('resp_max_key',.4,pch=4,cex=1.5)
+add_event('resp_central_key',0,pch=4,cex=1.5)
+add_event('resp_mel_key',-.4,pch=4,cex=1.5)
 # add_event('trial_start',.75,pch=24)
 # add_event('trial_end',.7,pch=25)
 # add_event('mel_light_on',.4,pch=24,bg='#33cc00cc')
